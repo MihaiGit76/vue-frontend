0053b0ff9b95af3cefe726762244b8ff2512bec1
MihaiGit76 <mihaiserbanus@yahoo.com>
2025-12-09 20:42:26 +0000

    Fix merge conflict in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-frontend/'
-    : '/'
-};
-=======
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
->>>>>>> 23716fe (Initial commit for Vue frontend)
+  publicPath: "./"
+}
